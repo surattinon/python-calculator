76445e0a98e1cfe59851486276a90386302178cb
surattinon <surattinon.basu@gmail.com>
2024-04-11 02:08:53 +0700

    conflict test 02

--- main.py
+++ main.py
@@ -31,11 +31,12 @@ b = 48
 a + b = c
 print(c)
 
-# From basu02 branch commit 03
+# From conflict basu02 branch commit 04
 print("From branch \"basu02\"") # conflict test
-a = 55
-b = 48
-a + b = c
+a = 59
+b = 41
+d = 60
+a + b + d = c
 print(c)
 
     user_operation = input()
